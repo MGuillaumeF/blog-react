784e89bbb4dc0fadc8fb273d5631f870a88af714
MGuillaumeF <michel.guillaume--foucaud@hotmail.fr>
2020-03-28 02:54:44 +0100

    Updates

---
+++ precache-manifest.1b3f9c7879bc9b476bb514324736d4c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9c09d3616e3cc2165049a4a04601f9",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "d2907122c635e4af4881",
+    "url": "/blog-react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f5c49eb6547066bff640",
+    "url": "/blog-react/static/css/main.da31a3a8.chunk.css"
+  },
+  {
+    "revision": "d2907122c635e4af4881",
+    "url": "/blog-react/static/js/2.c8a23b85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog-react/static/js/2.c8a23b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c49eb6547066bff640",
+    "url": "/blog-react/static/js/main.9ff805d4.chunk.js"
+  },
+  {
+    "revision": "5fbec72294d04f65d99b",
+    "url": "/blog-react/static/js/runtime-main.0baf70e8.js"
+  }
+]);
